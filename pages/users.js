c66892565a1a70dc542a0dbcf5ec28c8c143b16b
Kevin Owino <kowino@STIMA-SAC.COM>
2023-01-04 18:33:23 +0300

    Added component as part of page

--- pages/users.js
+++ pages/users.js
@@ -6,7 +6,7 @@ function UserList({ users }) {
     {
         users.map(user => {
             return(
-                <div key={user.id} class="pt">
+                <div key={user.id} className="pt">
                     <User user={ user} />
                 </div>
             )
@@ -20,7 +20,7 @@ export default UserList
 export async function getStaticProps() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await response.json()
-    console.log(data)
+    //console.log(data)
     
     return {
         props: {
